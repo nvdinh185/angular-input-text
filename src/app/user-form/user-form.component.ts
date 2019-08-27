2afe5c697075d9a79e8fae1abafefeb0768ca787
nvdinh185 <nvdinh0905671805@gmail.com>
2019-08-27 21:23:38 +0700

    first commit

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent {
+  name = '';
+  constructor() { }
+
+  showEvent(event) {
+    this.name = event.target.value;
+  }
+
+}
